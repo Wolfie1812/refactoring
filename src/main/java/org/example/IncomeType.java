e554e1b94769768f9ede13a380e5c8112be22861
Ileen de Wolf <ileen.de.wolf@nn.nl>
2024-04-25 14:01:25 +0200

    refactoring

--- src/main/java/org/example/IncomeType.java
+++ src/main/java/org/example/IncomeType.java
@@ -1,7 +1,11 @@
 package org.example;
 
 enum IncomeType {
-    CAR_WASHING("Car washing", 15), GARDENING("Gardening", 25), GIFTS("Gifts", 0), DISHWASHING("Dishwashing", 0);
+    CAR_WASHING("Car washing", 15),
+    GARDENING("Gardening", 25),
+    GIFTS("Gifts", 0),
+    DISHWASHING("Dishwashing", 0);
+
     private final String incomeName;
     private final int goal;
 
